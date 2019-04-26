26272d52da0e7cdddb731498c3091230e15d0135
Nick Rook <rooknj@outlook.com>
2019-04-26 16:19:16 -0400

    added docker-scripts

---
+++ src/docker-scripts.js
@@ -0,0 +1,68 @@
+const parseArgs = require("minimist");
+const spawn = require("cross-spawn");
+
+module.exports = (imageName, version) => {
+  const IMAGE_NAME = imageName;
+  const VERSION = version;
+
+  const getDockerImage = tag => {
+    let dockerTag = tag;
+    if (process.env.TRAVIS) {
+      console.log("In CI");
+      const branchName = process.env.TRAVIS_BRANCH;
+      if (branchName) {
+        if (branchName === "master") {
+          dockerTag = VERSION;
+        } else {
+          dockerTag = "test";
+        }
+      }
+    }
+    return `${IMAGE_NAME}:${dockerTag}`;
+  };
+
+  const buildDockerImage = async tag => {
+    const image = getDockerImage(tag);
+    console.log("Building", image);
+    const child = spawn("docker", ["build", "-t", image, "."], {
+      stdio: ["inherit", "inherit", "inherit"],
+      cwd: process.cwd(),
+      env: process.env
+    });
+    child.on("exit", code => {
+      if (code !== 0) process.exit(code);
+    });
+  };
+
+  const publishDockerImage = async tag => {
+    const image = getDockerImage(tag);
+    console.log("Publishing", image);
+    const child = spawn("docker", ["push", image], {
+      stdio: ["inherit", "inherit", "inherit"],
+      cwd: process.cwd(),
+      env: process.env
+    });
+    child.on("exit", code => {
+      if (code !== 0) process.exit(code);
+    });
+  };
+
+  // Process all command line arguments
+  return async argv => {
+    const args = parseArgs(argv.slice(2));
+    // a tag is required if not in CI
+    if (!args.t && !process.env.TRAVIS) {
+      console.log("Not in CI and no tag was given. Aborting");
+      process.exit(1);
+    }
+
+    if (args._.find(arg => arg === "build")) {
+      buildDockerImage(args.t);
+    } else if (args._.find(arg => arg === "publish")) {
+      publishDockerImage(args.t);
+    } else {
+      console.log("No valid options supplied. Aborting");
+      process.exit(1);
+    }
+  };
+};
